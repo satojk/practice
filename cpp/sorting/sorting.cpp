ca9524a5b3eb718e95c73a1a450d5fcb6bea1c24
Sato <lucasjks@gmail.com>
2017-08-10 02:11:27 -0300

    Add Bubble Sort and testing to sorting.cpp
    Also: fix Selection Sort

--- cpp/sorting/sorting.cpp
+++ cpp/sorting/sorting.cpp
@@ -3,6 +3,28 @@
 
 const int length = 5;
 
+struct Test
+{
+    int unsorted[length];
+    int sorted[length];
+};
+
+Test test1 = { { 1, 5, 2, 4, 3 }, { 1, 2, 3, 4, 5 } };
+Test test2 = { { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 } };
+Test test3 = { { 5, 4, 3, 2, 1 }, { 1, 2, 3, 4, 5 } };
+Test test4 = { { 2, 4, 3, 1, 5 }, { 1, 2, 3, 4, 5 } };
+Test tests[4] = { test1, test2, test3, test4 };
+
+bool compare(int arr1[length], int arr2[length])
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (arr1[i] != arr2[i])
+            return false;
+    }
+    return true;
+}
+
 void selectionSort(int arr[length])
 {
     for (int i = 0; i < length; i++)
@@ -10,7 +32,7 @@ void selectionSort(int arr[length])
         int minInd = i;
         for (int j = i; j < length; j++)
         {
-            if (arr[j] < arr[i])
+            if (arr[j] < arr[minInd])
             {
                 minInd = j;
             }
@@ -19,12 +41,29 @@ void selectionSort(int arr[length])
     }
 }
 
+void bubbleSort(int arr[length]) //unoptimized
+{
+    bool isUnsorted = true;
+    while (isUnsorted)
+    {
+        isUnsorted = false;
+        for (int i = 0; i < length - 1; i ++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                std::swap(arr[i], arr[i+1]);
+                isUnsorted = true;
+            }
+        }
+    }
+}
+
 int main()
 {
-    int arr[length] = { 50, 40, 30, 20, 10 };
-    selectionSort(arr);
-    for (int i = 0; i < length; i++)
-        std::cout << arr[i] << " ";
-    std::cout << std::endl;
+    for (int i = 0; i < 4; i++)
+    {
+        bubbleSort(tests[i].unsorted);
+        std::cout << (compare(tests[i].unsorted, tests[i].sorted) ? "Success" : "Fail") << std::endl;
+    }
     return 0;
 }
